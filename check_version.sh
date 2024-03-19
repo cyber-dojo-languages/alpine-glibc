b14428610afbf878cd66b2c09c5174ae3173bf68
Faye <faye@kosli.com>
2024-03-19 16:42:21 +0100

    update url for Alpine-glibc in install.sh and version number

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=3.16
+readonly EXPECTED="Welcome to Alpine Linux 3.19"
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'cat /etc/issue')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
