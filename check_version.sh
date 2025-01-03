df97ce724bfaae7e713ecce41def8b27bd977830
Faye <faye@kosli.com>
2025-01-03 15:09:51 +0100

    Remove -t from docker run in check_version.sh

--- check_version.sh
+++ check_version.sh
@@ -7,7 +7,7 @@ readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly EXPECTED="Welcome to Alpine Linux 3.21"
-readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'cat /etc/issue')
+readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'cat /etc/issue')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
